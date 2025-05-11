12f4c47e2b364401f12744126c5fbcda27d0bbc1
Andrew Dolgov <fox@fakecake.org>
2025-05-11 10:34:36 +0300

    adjust layout of compact headline rows, disable dividers for now

--- org.fox.ttrss/src/main/java/org/fox/ttrss/HeadlinesFragment.java
+++ org.fox.ttrss/src/main/java/org/fox/ttrss/HeadlinesFragment.java
@@ -358,12 +358,12 @@ public class HeadlinesFragment extends androidx.fragment.app.Fragment {
 		m_list.setLayoutManager(m_layoutManager);
 		m_list.setItemAnimator(new DefaultItemAnimator());
 
-		if (m_compactLayoutMode) {
+		/* if (m_compactLayoutMode) {
 			MaterialDividerItemDecoration materialDivider = new MaterialDividerItemDecoration(m_list.getContext(), m_layoutManager.getOrientation());
 			materialDivider.setDividerInsetStart(dpToPx(80));
 
 			m_list.addItemDecoration(materialDivider);
-		}
+		} */
 
 		ArticleListAdapter adapter = new ArticleListAdapter(getActivity(), R.layout.headlines_row, m_articles);
 
@@ -795,7 +795,6 @@ public class HeadlinesFragment extends androidx.fragment.app.Fragment {
 			menuButtonView = v.findViewById(R.id.article_menu_button);
 			flavorImageHolder = v.findViewById(R.id.flavorImageHolder);
 			flavorImageLoadingBar = v.findViewById(R.id.flavorImageLoadingBar);
-			headlineFooter = v.findViewById(R.id.headline_footer);
 			textImage = v.findViewById(R.id.text_image);
 			textChecked = v.findViewById(R.id.text_checked);
 			headlineHeader = v.findViewById(R.id.headline_header);
@@ -1190,6 +1189,12 @@ public class HeadlinesFragment extends androidx.fragment.app.Fragment {
 
 					holder.excerptView.setTextSize(TypedValue.COMPLEX_UNIT_SP, headlineFontSize);
 					holder.excerptView.setText(excerpt);
+
+					if (!excerpt.isEmpty()) {
+						holder.excerptView.setVisibility(View.VISIBLE);
+					} else {
+						holder.excerptView.setVisibility(View.GONE);
+					}
 
 					if (!canShowFlavorImage()) {
 						holder.excerptView.setPadding(holder.excerptView.getPaddingLeft(),
